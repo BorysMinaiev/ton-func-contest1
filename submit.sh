d7cf60a12c1ff925abe3497893cd3b0bc66ca35c
Borys <mb.787788@gmail.com>
2022-04-07 14:03:18 +0100

    submit script

---
+++ submit.sh
@@ -0,0 +1,8 @@
+mkdir -p submit
+cp task-1/func/code.fc submit/1.fc
+cp task-2/func/code.fc submit/2.fc
+cp task-3/func/code.fc submit/3.fc
+cp task-4/func/code.fc submit/4.fc
+cp task-5/func/code.fc submit/5.fc
+cp participant.json submit/participant.json
+tar cvf submit.tar submit/*
